87c4336e41a6a67311609d7e3d74b630a7fb591e
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2021-03-14 14:51:37 -0300

    Initial Commit

---
+++ application/controllers/restrita/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('restrita/layout/header');
+        $this->load->view('restrita/home/index');
+        $this->load->view('restrita/layout/footer');
+    }
+}
